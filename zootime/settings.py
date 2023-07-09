dd77481dd2612da8ccf88b0b5123ea8c1a8eddf4
mandaringale <ingale.mandar@outlook.com>
2023-07-09 09:51:53 +0800

    Commented Storage

--- zootime/settings.py
+++ zootime/settings.py
@@ -184,14 +184,14 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 
 
-DEFAULT_FILE_STORAGE = 'core.azure_storage.AzureMediaStorage'
-STATICFILES_STORAGE = 'core.azure_storage.AzureStaticStorage'
-AZURE_ACCOUNT_NAME = os.getenv('AZURE_ACCOUNT_NAME')
-AZURE_ACCOUNT_KEY = os.getenv('AZURE_ACCOUNT_KEY')
-AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
+# DEFAULT_FILE_STORAGE = 'core.azure_storage.AzureMediaStorage'
+# STATICFILES_STORAGE = 'core.azure_storage.AzureStaticStorage'
+# AZURE_ACCOUNT_NAME = os.getenv('AZURE_ACCOUNT_NAME')
+# AZURE_ACCOUNT_KEY = os.getenv('AZURE_ACCOUNT_KEY')
+# AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
 
-STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/static/'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/media/'
-MEDIA_ROOT = BASE_DIR / 'mediafiles'
+# MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/media/'
+# MEDIA_ROOT = BASE_DIR / 'mediafiles'
